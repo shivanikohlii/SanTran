372fc7fa941e51bd1e4b882689f0a42385219776
Shivani Kohli <shivanikohli.09@gmail.com>
2018-04-09 18:52:27 -0500

    Data files

---
+++ scrapeGita.py
@@ -0,0 +1,89 @@
+# import libraries
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+import bs4
+import unicodedata
+import codecs
+import string
+import re
+import csv
+
+
+
+def getGita(chap, verse):
+    quote_page = "https://www.holy-bhagavad-gita.org/chapter/"+str(chap)+"/verse/" + str(verse)
+    req = Request(quote_page, headers={'User-Agent': 'Mozilla/5.0'})
+    # query the website and return the html to the variable ‘page’
+    try:
+        page = urlopen(req).read()
+    except Exception as e:
+        print("page that failed was: " + quote_page)
+        raise e
+    
+    # parse the html using beautiful soup and store in variable `soup`
+    soup = BeautifulSoup(page, 'html.parser')
+    
+    
+    sa = soup.find(id="originalVerse") #finds sanskrit
+    pTagSA = sa.p
+    pTagSA = [str(elm) for elm in pTagSA.contents if (type(elm) == bs4.element.NavigableString)]#this only keeps strings from the HTML
+    #print(pTagSA)
+    
+    
+    SA = ''.join(re.sub(r"\d+\|\|","",str(elem)) for elem in pTagSA) # this removes the | characters found in the sanskrit
+    SA = ''.join([i for i in SA if not i.isdigit()]) #idk if this is needed but it takes random verse numbers from the end of the sanskrit
+    #print()
+    #print(SA)
+    
+    #-----------------------------------------
+    
+    en = soup.find(id="translation") #finds the english
+    pTagEN = en.p
+    #for con in pTagEN.contents:
+    #    if type(con) == bs4.element.Tag:
+    #        print(str(con.get('id'))+"-"+str(con))
+
+    #pTagEN = [str(elm) for elm in pTagEN.contents if (type(elm) == bs4.element.NavigableString) ] #this only keeps strings from the HTML
+    enL = []
+    for elm in pTagEN.contents:
+        strElm = str(elm)
+        if (type(elm) == bs4.element.NavigableString):
+            enL.append(strElm)
+        if type(elm) == bs4.element.Tag:
+            if "<i>" in strElm: 
+                strElm = re.sub(r"\</i\>","",strElm)
+                strElm = re.sub(r"\<i\>","",strElm)
+                enL.append(strElm)
+
+    #print(pTagEN)
+    #print()
+    EN = ''.join(str(elm).replace('\n','') for elm in enL)
+    #EN = pTagEN[1].replace('\n','') #removes newlines from the english
+    
+    #this gets rid of the punctuation 
+    #exclude = set(string.punctuation)                   
+    #EN = ''.join(ch for ch in EN if ch not in exclude)
+    
+    #print(EN)
+    mId = "c:"+str(chap)+"v"+str(verse)
+    return (mId,SA,EN)
+
+
+versesInChap = [46,72,43,42,29,47,30,28,34,42,55,20,35,27,20,24,28,78] 
+
+#print(getGita(5,3))
+
+with open('data.csv','w') as out:
+    csv_out=csv.writer(out)
+    csv_out.writerow(["id","SA","EN"])
+    for i in range(1, 19): #chap
+        for j in range(1, versesInChap[i-1]+1): #verse
+            print("chap: " +str(i)+"verse: " +str(j))
+            csv_out.writerow(getGita(i,j))
+
+
+
+
+
+
+
---
+++ scrapeit.py
@@ -0,0 +1,84 @@
+
+import json, urllib.request, csv
+
+def readurl(url, **kwargs):
+    return list(urllib.request.urlopen(url, **kwargs))[0]
+
+def search_dict_by(term, search_value, dict):
+    return dict[list(filter(lambda k: dict[k][term] == value, dict))[0]]
+    
+def get_index_data():
+    """Return a dictionary mapping book names to {book id, name, chapter_count}."""
+
+    json_contents = json.loads(readurl('http://sanskritbible.in/assets/php/scan-index.php'))
+    books = json_contents[1]
+    # map a book name to its number and number of books
+    books = dict(map(lambda book_json: (book_json["0"], {"book_id": book_json["0"], "name": book_json["1"], "chapter_count": book_json["3"]}), books))
+    # only gospels are translated so far
+    # matthew = search_dict_by("name", "Matthew", books)
+    # acts = search_dict_by("name", "Acts", books)
+    # books = dict(map(lambda k: (k, books[k]), filter(lambda k: books[k]["book_id"] >= matthew["book_id"] and books[k]["book_id"] <= acts["book_id"], books)))
+    
+    return books
+    
+    
+def get_chapter_data(book_id, chapter):
+    """Return a dictionary mapping data id's to {data_id, book_id, chapter, verse, sanskrit, english}"""
+    
+    request_data = {
+        "BookNo": str(book_id),
+        "ChapterNo": str(chapter)
+    }
+    json_contents = json.loads(readurl('http://sanskritbible.in/assets/php/read-btxt.php', data = bytes(json.dumps(request_data), 'utf-8')))
+    if json_contents[0] is None:
+        return {}
+    
+    sanskrit_data = json_contents[0]
+    english_data = json_contents[4]
+    
+    data = dict(map(lambda sanskrit_json: (sanskrit_json["0"], {
+        "data_id": sanskrit_json["0"],
+        "book_id": sanskrit_json["1"],
+        "chapter": sanskrit_json["2"],
+        "verse": sanskrit_json["3"], 
+        "sanskrit": sanskrit_json["4"].encode('unicode_escape').decode('utf-8')
+    }), sanskrit_data))
+    for english_json in english_data:
+        data[english_json["0"]]["english"] = english_json["4"]
+        
+    return data
+    
+def get_all_data():
+    """Return a unified version of get_chapter_data for all available chapters.
+    (takes a while to run - progress printed to stdout)"""
+
+    index_data = get_index_data()
+    all_data = {}
+
+    for book_id in index_data:
+        book = index_data[book_id]
+        for chapter in range(1, book["chapter_count"]):
+            print('{} {}'.format(book["name"], chapter))
+            data = get_chapter_data(book_id, chapter)
+            if data == {}:
+                print('Empty')
+                break # empty chapter, no more data in this book probably
+            all_data.update(get_chapter_data(book_id, chapter))
+    
+    return all_data
+    
+    
+def write_data_to_csv(data, file):
+    """Write a dictionary of object dictionaries (such as those from get_all_data) to a csv file."""
+    data_cols = list(data[list(data)[0]])
+    rows = [data_cols] + [[data[data_key][col] for col in data_cols] for data_key in data]
+
+    with open(file, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(rows)
+    
+if __name__ == '__main__':
+    index_data = get_index_data()
+    write_data_to_csv(index_data, 'books.csv')
+    data = get_all_data()
+    write_data_to_csv(data, 'data.csv')
